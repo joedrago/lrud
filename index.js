cf905ca62f1093cda1cb2f52d046c435de939540
Joe Drago <joedrago@gmail.com>
2023-07-09 15:33:47 -0700

    Debug text

--- index.js
+++ index.js
@@ -41,8 +41,11 @@ const highlightCoords = () => {
         c.distSq = ((srcX - c.x) * (srcX - c.x)) + ((srcY - c.y) * (srcY - c.y))
         c.angle = Math.atan2(c.y - srcY, c.x - srcX) * 180 / Math.PI
 
-        c.element.innerHTML = `${directionFromAngle(c.angle)}`
-        // c.element.innerHTML = `${directionFromAngle(c.angle)} ${c.angle.toFixed(2)}` // Debug Text
+        if(window.location.hash == "#d") {
+            c.element.innerHTML = `${directionFromAngle(c.angle)}`
+        } else if(window.location.hash == "#a") {
+            c.element.innerHTML = `${directionFromAngle(c.angle)} ${c.angle.toFixed(2)}`
+        }
 
         if(selected.id != c.id) {
             c.element.classList.add(directionFromAngle(c.angle))
